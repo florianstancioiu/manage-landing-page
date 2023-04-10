aed94184240a04bb824bbbdbaf23b17d5bfba5a7
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-04-10 10:29:17 +0300

    Worked on mobile simplify section

--- src/components/SimplifySection.jsx
+++ src/components/SimplifySection.jsx
@@ -1,5 +1,13 @@
 import classes from './SimplifySection.module.css';
+import Button from './UI/Button';
 
-const SimplifySection = () => {};
+const SimplifySection = () => {
+  return (
+    <div className={classes.simplify}>
+      <p className={classes.title}>Simplify how your team works today.</p>
+      <Button title='Get Started' alternate />
+    </div>
+  );
+};
 
 export default SimplifySection;
