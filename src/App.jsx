c15c858339f113a79c15055e368058b168d50b70
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-04-10 11:28:36 +0300

    Added mobile header bg image

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <>
-      <header>
+      <header className='header'>
         <Header />
         <Hero />
       </header>
