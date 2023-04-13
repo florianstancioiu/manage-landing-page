68cf2490bfcd638c5d22626bca55659ea13b6bcc
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-04-13 15:21:22 +0300

    Animated about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,9 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { useInView, motion, useAnimation } from 'framer-motion';
+
 import classes from './AboutSection.module.css';
 import AboutSectionItem from './AboutSectionItem';
 
 const AboutSection = () => {
   const [items, setItems] = useState([]);
+  const differentWrapperControl = useAnimation();
+  const differentWrapperRef = useRef(null);
+  const differentWrapperInView = useInView(differentWrapperRef);
+  const diffrenteWrapperVariants = {
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: { duration: 0.5 },
+    },
+    hidden: {
+      x: -100,
+      opacity: 0,
+    },
+  };
 
   useEffect(() => {
     fetch('about-items.json')
@@ -11,16 +27,30 @@ const AboutSection = () => {
       .then((data) => setItems(data.items));
   }, []);
 
+  useEffect(() => {
+    if (differentWrapperInView) {
+      differentWrapperControl.start('visible');
+    } else {
+      differentWrapperControl.start('hidden');
+    }
+  }, [differentWrapperInView, differentWrapperControl]);
+
   return (
     <div className={classes.wrapper}>
-      <div className={classes['different-wrapper']}>
+      <motion.div
+        ref={differentWrapperRef}
+        variants={diffrenteWrapperVariants}
+        initial='hidden'
+        animate={differentWrapperControl}
+        className={classes['different-wrapper']}
+      >
         <h2 className={classes.title}>What’s different about Manage?</h2>
         <p className={classes.description}>
           Manage provides all the functionality your team needs, without the
           complexity. Our software is tailor-made for modern digital product
           teams.
         </p>
-      </div>
+      </motion.div>
       <div className={classes['about-items-wrapper']}>
         {items.map((item, index) => (
           <AboutSectionItem
--- src/components/AboutSectionItem.jsx
+++ src/components/AboutSectionItem.jsx
@@ -1,14 +1,39 @@
+import { useEffect, useRef } from 'react';
+import { useInView, motion, useAnimation } from 'framer-motion';
+
 import classes from './AboutSectionItem.module.css';
 
 const AboutSectionItem = ({ id, title, desc }) => {
+  const control = useAnimation();
+  const ref = useRef(null);
+  const inView = useInView(ref);
+  const variants = {
+    visible: { x: 0, opacity: 1, transition: { duration: 0.5 } },
+    hidden: { x: 100, opacity: 0 },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      control.start('visible');
+    } else {
+      control.start('hidden');
+    }
+  }, [inView, control]);
+
   return (
-    <div className={classes.wrapper}>
+    <motion.div
+      ref={ref}
+      variants={variants}
+      initial='hidden'
+      animate={control}
+      className={classes.wrapper}
+    >
       <div className={classes.title}>
         <span className={classes.id}>{id}</span>
         <span className={classes['actual-title']}>{title}</span>
       </div>
       <div className={classes.desc}>{desc}</div>
-    </div>
+    </motion.div>
   );
 };
 
